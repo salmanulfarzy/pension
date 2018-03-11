49bc4b030203b70596b1ae2b3bb0ef66edfd385c
Salmanul Farzy <salmanulfarzy@gmail.com>
2018-03-11 16:32:08 +0530

    Calculate total_service and qualifying service
    Not considering any non-qualifying services or special services. They
    will be added in later iteration

---
+++ main.js
@@ -0,0 +1,15 @@
+function calculate() {
+	const date_of_joining = document.getElementById('doj').valueAsDate;
+	const date_of_retirement = document.getElementById('dor').valueAsDate;
+	years = date_of_retirement.getFullYear() - date_of_joining.getFullYear();
+	months = date_of_retirement.getMonth() - date_of_joining.getMonth();
+	days = date_of_retirement.getDate() - date_of_joining.getDate();
+
+	total_service_text = document.createTextNode(
+		`${years} Years ${months} Months and ${days} Days`);
+	// months are zero indexed
+	qualifying_service = document.createTextNode(`${months > 5 ? years+1 : years} Years`);
+
+	document.getElementById('ts').appendChild(total_service_text);
+	document.getElementById('qs').appendChild(qualifying_service);
+}
