923e12c3d062cd3a2568fda826a9d52fdc0b2254
Salmanul Farzy <salmanulfarzy@gmail.com>
2018-03-11 16:58:59 +0530

    Separate qualifying service var and text

--- main.js
+++ main.js
@@ -5,11 +5,12 @@ function calculate() {
 	months = Math.abs(date_of_retirement.getMonth() - date_of_joining.getMonth());
 	days = Math.abs(date_of_retirement.getDate() - date_of_joining.getDate());
 
-	total_service_text = document.createTextNode(
-		`${years} Years ${months} Months and ${days} Days`);
+	total_service_text = document.createTextNode(`${years} Years ${months} Months and ${days} Days`);
+
 	// months are zero indexed
-	qualifying_service = document.createTextNode(`${months > 5 ? years+1 : years} Years`);
+	qualifying_service = months > 5 ? years+1 : years;
+	qualifying_service_text = document.createTextNode(`${qualifying_service} Years`);
 
 	document.getElementById('ts').appendChild(total_service_text);
-	document.getElementById('qs').appendChild(qualifying_service);
+	document.getElementById('qs').appendChild(qualifying_service_text);
 }
